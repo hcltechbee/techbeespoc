45466f7b0e5800c1ea6d8d6d44b6f54c5e7c4233
lekhanshuofficialgit <73520954+lekhanshuofficialgit@users.noreply.github.com>
2020-11-09 14:21:09 +0530

    Delete dbConstants.java

--- src/com/constants/dbConstants.java
+++
@@ -1,7 +0,0 @@
-package com.constants;
-
-public class dbConstants {
-	public static final String connectionUrl = "jdbc:mysql://localhost:3309/users";
-	public static final String connectionUsername = "root";
-	public static final String connectionPassword = "123456";
-}

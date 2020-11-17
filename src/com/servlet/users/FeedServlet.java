db1fd9aeaaf02eb6a4dc4b8b4886d58045868709
KESHAV-CHOUDHARY-hub <73520580+KESHAV-CHOUDHARY-hub@users.noreply.github.com>
2020-11-17 19:52:42 +0530

    Create FeedServlet.java

---
+++ src/com/servlet/users/FeedServlet.java
@@ -0,0 +1,62 @@
+package com.servlet.users;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.classes.users.UsersDataBase;
+import com.constants.UIConstants;
+
+public class FeedServlet extends HttpServlet {
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		
+		PrintWriter out=response.getWriter();
+		
+		HttpSession session=request.getSession();
+		session = request.getSession(false);
+		
+		
+		String loginedUser="";
+        String loginedUserId="";
+        int CheckId=0;
+        if(session.getAttribute(com.constants.UIConstants.sessionName)!= null)
+        {
+        	loginedUser =(String)session.getAttribute(com.constants.UIConstants.sessionName);
+        	loginedUserId=(String)session.getAttribute(com.constants.UIConstants.sessionUser_id);
+        	CheckId=Integer.parseInt(loginedUserId);
+        	String userLogined[]=loginedUser.split(" ");
+        	//System.out.println(userLogined[0]);
+        	loginedUser=userLogined[0];
+        	//System.out.println(loginedUser);	
+        } 
+        System.out.println(CheckId);
+        
+		String messageOfUser ="";
+	    messageOfUser=request.getParameter("messageByUser");
+		String imageOfUser = "";
+		imageOfUser=request.getParameter("image");
+		
+		if(messageOfUser.isBlank()) {
+			out.println("<script type=\"text/javascript\">");  
+			out.println("alert('PLEASE ENTER SOME TEXT');");  
+			out.println("</script>"); 
+		}
+		else if(imageOfUser.isEmpty()) {
+			out.println("<script type=\"text/javascript\">");  
+			out.println("alert('NO PHOTO SECLETED');");  
+			out.println("</script>");
+		}
+		
+		else {
+		UsersDataBase.InsertFeed(CheckId, messageOfUser, imageOfUser);
+		response.sendRedirect("Home.jsp");
+		}
+	
+		
+	}	
+}

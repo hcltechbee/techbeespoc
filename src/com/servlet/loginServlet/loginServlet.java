d6854f6adf5bc3526eee8cc0435161e34800f3e6
Richa-srivastava1712 <58876904+Richa-srivastava1712@users.noreply.github.com>
2020-12-07 10:55:26 +0530

    Update loginServlet.java

--- src/com/servlet/loginServlet/loginServlet.java
+++ src/com/servlet/loginServlet/loginServlet.java
@@ -1,10 +1,19 @@
 package com.servlet.loginServlet;
 
 import java.io.IOException;
+import java.math.BigInteger;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
-
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,61 +21,68 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
+import org.mindrot.jbcrypt.BCrypt;
 import com.classes.users.UsersDataBase;
+import com.connection.Userdetail;
+import com.connection.Userslogin;
 import com.constants.QueryConstants;
+import com.servlet.users.Userfeed;
 
-
-
-/**
- * Servlet implementation class loginServlet
- */
 @WebServlet("/loginServlet")
-public class loginServlet extends HttpServlet {    
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+public class loginServlet extends HttpServlet {
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		try {
 			String email = request.getParameter(com.constants.UIConstants.LOGIN_EMAIL);
 			String password = request.getParameter(com.constants.UIConstants.LOGIN_PASSWORD);
-			Connection con = UsersDataBase.getConnection();
-			Statement stmt = con.createStatement();
-			String query =  QueryConstants.CHECKEMAIL+ email + "'"; 
-			ResultSet rs = stmt.executeQuery(query);
-			String userEmail, userPassword, userName, lastName, firstName, userId;
-			
-			Boolean isAdmin;
+			List<Userslogin> login = new ArrayList<>();
+			String userEmail = "", userPassword = "", userName = "", lastName = "", firstName = "";
+			Userslogin user1 = new Userslogin();
+			EntityManagerFactory emf = Persistence.createEntityManagerFactory("InsertUsers");
+			EntityManager em = emf.createEntityManager();
+			Query query = em.createQuery("SELECT u FROM Userslogin u where u.emailId  = " + "\"" + email + "\"");
+			login = query.getResultList();
+			em.getTransaction().begin();
+			int isAdmin = 0;
+			int userId = 1;
+			System.out.println("hi");
+			for (Userslogin s : login) {
+				userEmail = s.getEmailId();
+				System.out.println(userEmail);
+				userPassword = s.getPassword();
+
+				firstName = s.getFirstName();
+				isAdmin = s.getIsadmin();
+				lastName = s.getLastName();
+				userId = s.getUser_Id();
+			}
+			System.out.println("hi 1");
+			em.getTransaction().commit();
+			emf.close();
 			String success = "false";
-			if(rs.next()){
-				userEmail = rs.getString(1);
-				userPassword = rs.getString(2);
-				firstName = rs.getString(3);
-				isAdmin = rs.getBoolean(4);
-				lastName = rs.getString(5);
-				userId=rs.getString(6);
-				//boolean matched = BCrypt.checkpw(password, userPassword);
-				//System.out.println("Passwd matched : "+matched);
-			if(userPassword.equals(password)){
-				success = "true";
-				response.sendRedirect(com.constants.URLConstants.LOGIN_SUCCESS_URL + success);
-				HttpSession session = request.getSession();	
-				session.setAttribute(com.constants.UIConstants.SESSION_NAME, firstName+" "+lastName);	
-				session.setAttribute(com.constants.UIConstants.SESSION_ISADMIN, userId);
-				session.setAttribute(com.constants.UIConstants.SESSION_ISADMIN, isAdmin);
-				session.setAttribute(com.constants.UIConstants.SESSION_LOGGEDIN, true);
-			}
-			else{
-				success = "false";
-				System.out.println("sorry");
-				response.sendRedirect(com.constants.URLConstants.LOGIN_SUCCESS_URL + success);
-			}
-			}
-			else{
-				success="username_incorrect";
+			if (userEmail != null) {
+				boolean matched = BCrypt.checkpw(password, userPassword);
+				System.out.println("Hash Password matched : " + matched);
+				if (matched == true) {
+					success = "true";
+					response.sendRedirect(com.constants.URLConstants.LOGIN_SUCCESS_URL + success);
+					HttpSession session = request.getSession();
+					session.setAttribute(com.constants.UIConstants.SESSION_NAME, firstName + " " + lastName);
+					session.setAttribute(com.constants.UIConstants.SESSION_USER_ID, userId);
+					// System.out.println(""+userId);
+					session.setAttribute(com.constants.UIConstants.SESSION_ISADMIN, isAdmin);
+					session.setAttribute(com.constants.UIConstants.SESSION_LOGGEDIN, true);
+				} else {
+					success = "false";
+					System.out.println("sorry");
+					response.sendRedirect(com.constants.URLConstants.LOGIN_SUCCESS_URL + success);
+				}
+			} else {
+				success = "username_incorrect";
 				response.sendRedirect(com.constants.URLConstants.LOGIN_SUCCESS_URL + success);
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
 	}
-
 }
